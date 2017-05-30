58c5254086c158ad141eaf237344d5240285c6e1
mkbedi <manpreetkaurbedi@gail.com>
2017-05-30 14:07:32 +0530

    Adding files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Manpreet on 5/29/2017.
- */
-
-import React from "react";
-import {render} from "react-dom";
-import App from "./App";
-
-render(<App greeting="Vendor Management System"/>, document.getElementById('main'));
